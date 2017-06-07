0d4c4eede586da149de068b047466d78a496996a
Timmy <timmy@air.zyxel.com>
2017-06-07 22:35:53 +0200

    Initial Commit

---
+++ CustomSoundBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CustomSoundBar
+//
+//  Created by Timmy on 07.06.17.
+//  Copyright © 2017 Timothy Mc Alister. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CustomSoundBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CustomSoundBar
+//
+//  Created by Timmy on 07.06.17.
+//  Copyright © 2017 Timothy Mc Alister. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
